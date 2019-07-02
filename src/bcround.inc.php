23bafe159a0ff56ef008e7695c2dca07d8218eeb
Wes <netmosfera@gmail.com>
2019-07-02 02:39:48 +0200

    Improve readability and add normalization

--- src/bcround.inc.php
+++ src/bcround.inc.php
@@ -4,5 +4,7 @@ namespace PHPToolBucket\BCMathTools;
 
 function bcround(String $number, Int $scale = 0){
     $sign = bccomp($number, "0") > 0 ? "+" : "-";
-    return bcadd($number, $sign . "0." . str_repeat("0", $scale) . "5", $scale);
+    $addend = $sign . "0." . str_repeat("0", $scale) . "5";
+    return bcadd(bcadd($number, $addend, $scale), "0", $scale);
+    // ^ The second bcadd makes sure that things like "-0" are normalized before returning
 }
